56a78a4806e9233ad95d531a440cffb6ddb4ce47
Luca <lukkino31@gmail.com>
2022-10-30 14:57:16 +0100

    adding operation buttons file, fixed digitButtons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import  "./styles.css";
 import { useReducer } from "react";
+import DigitButton from "./DigitButtons";
+import OperationButton from "./OperationButton";
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear',
@@ -14,7 +16,7 @@ function reducer(state, { type, payload }) {
     case ACTIONS.ADD_DIGIT:
       return {
         ...state,
-        currentOperand: `${currentOperand || ''}${payload.digit}`,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
   }
 }
@@ -30,20 +32,21 @@ const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(re
       </div>
     <button className='span-two'>AC</button>
     <button>DEL</button>
-    <button>÷</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>+</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>-</button>
-    <button>.</button>
-    <button>0</button>
+    <OperationButton operation='÷' dispatch={dispatch} />
+    <DigitButton digit='1' dispatch={dispatch} />
+    <DigitButton digit='2' dispatch={dispatch} />
+    <DigitButton digit='3' dispatch={dispatch} />
+    <OperationButton operation='*' dispatch={dispatch} />
+    <DigitButton digit='4' dispatch={dispatch} />
+    <DigitButton digit='5' dispatch={dispatch} />
+    <DigitButton digit='6' dispatch={dispatch} />
+    <OperationButton operation='+' dispatch={dispatch} />
+    <DigitButton digit='7' dispatch={dispatch} />
+    <DigitButton digit='8' dispatch={dispatch} />
+    <DigitButton digit='9' dispatch={dispatch} />
+    <OperationButton doperation='-' dispatch={dispatch} />
+    <DigitButton digit='.' dispatch={dispatch} />
+    <DigitButton digit='0' dispatch={dispatch} />
     <button className='span-two'>=</button>
     </div>
   )
